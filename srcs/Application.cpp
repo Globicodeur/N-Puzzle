154cbd4cff728d81f307b8472dda733194982bb4
Guillaume DEPARDON <guillaume.depardon@student.42.fr>
2015-05-09 01:40:28 +0200

    [Oh god] Added runtime heuristics composition

--- srcs/Application.cpp
+++ srcs/Application.cpp
@@ -7,28 +7,118 @@
 #include "algorithm/heuristics.hpp"
 #include "algorithm/heuristic_composition.hpp"
 
-Application::Application(int argc, char **argv) {
-    Options::parseFromCommandLine(argc, argv);
-}
+#include "tools/type_traits.hpp"
 
-template <uint size>
-using Heuristics = algorithm::heuristics::Composition<
-    // algorithm::heuristics::MisplacedRowsAndColumns,
-    algorithm::heuristics::ManhattanDistance
-    // algorithm::heuristics::LinearConflict
->::Composer<size>;
+// Forwards
+using Initial = const parsing::ParsedPuzzle &;
+using Goal = boost::optional<parsing::ParsedPuzzle>;
+using algorithm::heuristics::HEURISTICS_COUNT;
 
-void Application::run(void) {
-    std::cout << "Starting with: " << Options::inputFile << std::endl;
+template <std::size_t opt_i, std::size_t h_i, class Set>
+std::enable_if_t<h_i == HEURISTICS_COUNT - 1>
+addHeuristic(Initial, Goal);
 
-    parsing::Parser parser;
-    auto initialState = parser.parse(Options::inputFile);
+template <std::size_t opt_i, std::size_t h_i, class Set>
+std::enable_if_t<h_i != HEURISTICS_COUNT - 1>
+addHeuristic(Initial, Goal);
+//
 
-    algorithm::Solver<Heuristics> solver;
+template <HClass... Heuristics>
+void solve(Initial initial, Goal goal, std::tuple<tools::Wrapper<Heuristics>...>) {
+    using SolverT = algorithm::Solver<
+        algorithm::heuristics::Composition<
+            Heuristics...
+        >::template Composer
+    >;
+
+    SolverT solver;
 
     auto showResults = [](const auto & solution) {
         std::cout << solution << std::endl;
     };
+    solver.solve(initial, goal, showResults);
+}
 
-    solver.solve(initialState, showResults);
+template <std::size_t i, class Set>
+std::enable_if_t<(i > HEURISTICS_COUNT)>
+solveWithHeuristics(Initial, Goal) {
+    throw std::out_of_range { "Too many heuristics" };
+}
+
+template <std::size_t i, class Set>
+std::enable_if_t<
+    boost::mpl::size<Set>::type::value != 0 &&
+    i <= HEURISTICS_COUNT
+>
+solveWithHeuristics(Initial initial, Goal goal) {
+    using HeuristicsTuple = typename tools::Unify<
+        std::tuple,
+        Set,
+        boost::mpl::size<Set>::type::value
+    >::type;
+
+    if (i == Options::heuristics.size())
+        return solve(initial, goal, HeuristicsTuple { });
+    addHeuristic<i, 0, Set>(initial, goal);
+}
+
+template <std::size_t opt_i, std::size_t h_i, class Set>
+bool optionMatch(Initial initial, Goal goal) {
+    using NewSet = typename boost::mpl::insert<
+        Set,
+        tools::Wrapper<algorithm::heuristics::HeuristicAt<h_i>::template type>
+    >::type;
+
+    if (algorithm::heuristics::HeuristicAt<h_i>::name ==
+        Options::heuristics.at(opt_i)) {
+        solveWithHeuristics<opt_i + 1, NewSet>(initial, goal);
+        return true;
+    }
+    return false;
+}
+
+template <std::size_t opt_i, std::size_t h_i, class Set>
+std::enable_if_t<h_i == HEURISTICS_COUNT - 1>
+addHeuristic(Initial initial, Goal goal) {
+    if (!optionMatch<opt_i, h_i, Set>(initial, goal))
+        throw std::out_of_range { "Unknown heuristic" };
+}
+
+template <std::size_t opt_i, std::size_t h_i, class Set>
+std::enable_if_t<h_i != HEURISTICS_COUNT - 1>
+addHeuristic(Initial initial, Goal goal) {
+    if (!optionMatch<opt_i, h_i, Set>(initial, goal))
+        addHeuristic<opt_i, h_i + 1, Set>(initial, goal);
+}
+
+Application::Application(int argc, char **argv) {
+    Options::parseFromCommandLine(argc, argv);
+}
+
+template <std::size_t i = 0, HClass... Hs>
+std::enable_if_t<sizeof...(Hs) == 0>
+solveWithHeuristics(Initial initial, Goal goal) {
+    addHeuristic<0, 0, boost::mpl::set<>>(initial, goal);
+}
+
+void Application::run(void) {
+    parsing::Parser parser;
+    parsing::ParsedPuzzle initialState;
+    Goal goalState;
+
+    initialState = parser.parse(Options::initialFile);
+    if (!Options::goalFile.empty())
+        goalState = parser.parse(Options::goalFile);
+
+    // RUNTIME composition
+    solveWithHeuristics(initialState, goalState);
+
+    // STATIC composition (because fuck compilers)
+    using StaticHeuristics = std::tuple<
+          tools::Wrapper<algorithm::heuristics::ManhattanDistance>
+        , tools::Wrapper<algorithm::heuristics::LinearConflict>
+        // , tools::Wrapper<algorithm::heuristics::MisplacedTiles>
+        // , tools::Wrapper<algorithm::heuristics::MisplacedRowsAndColumns>
+    >;
+    solve(initialState, goalState, StaticHeuristics{});
 }
--- srcs/algorithm/heuristics.hpp
+++ srcs/algorithm/heuristics.hpp
@@ -4,3 +4,34 @@
 #include "heuristics/LinearConflict.hpp"
 #include "heuristics/MisplacedTiles.hpp"
 #include "heuristics/MisplacedRowsAndColumns.hpp"
+
+namespace algorithm {
+
+    namespace heuristics {
+
+        template <const char * n, template <uint> class H>
+        struct NamedHeuristic {
+            template <uint size> using type = H<size>;
+            static constexpr const char * name = n;
+        };
+
+        constexpr char linearName[]         = "linear";
+        constexpr char manhattanName[]      = "manhattan";
+        constexpr char misplacedName[]      = "misplaced";
+        constexpr char misplacedRCName[]    = "misplacedRC";
+
+        using Heuristics = std::tuple<
+            NamedHeuristic<manhattanName,   ManhattanDistance>,
+            NamedHeuristic<linearName,      LinearConflict>,
+            NamedHeuristic<misplacedName,   MisplacedTiles>,
+            NamedHeuristic<misplacedRCName, MisplacedRowsAndColumns>
+        >;
+
+        template <std::size_t i>
+        using HeuristicAt = typename std::tuple_element<i, Heuristics>::type;
+
+        constexpr auto HEURISTICS_COUNT = std::tuple_size<Heuristics>::value;
+
+    }
+
+}
