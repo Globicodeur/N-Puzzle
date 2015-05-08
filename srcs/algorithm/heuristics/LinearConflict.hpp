1cc7e439c4a75bba86e37bfa21b7c7c2f7e710a6
Kenny POIRIER <kpoirier@student.42.fr>
2015-05-08 15:21:14 +0200

    [Algorithm] Reworked linear conflict heuristic, now correct

--- srcs/algorithm/heuristics/LinearConflict.hpp
+++ srcs/algorithm/heuristics/LinearConflict.hpp
@@ -11,21 +11,55 @@ namespace algorithm {
 
             LinearConflict(const puzzle::Puzzle<size> & goal) {
                 for (uint i = 0; i < size * size; ++i)
-                    indexes[goal.grid[i]] = i;
+                    goalIndexes[goal.grid[i]] = i;
             }
 
             auto operator()(const puzzle::Puzzle<size> & puzzle) const {
                 uint cost = 0;
 
-                for (uint i = 0; i < size; ++i) {
-                    for (uint j = 1; j < size; ++j) {
-                        auto tj = puzzle.grid[(i * size) + j];
-                        auto tk = puzzle.grid[(i * size) + (j - 1)];
-                        auto gj = indexes[tj];
-                        auto gk = indexes[tk];
-                        bool sameLine = (gj / size == gk / size == i);
-                        bool conflict = (gj % size < gk % size);
-                        cost += (sameLine && conflict);
+                uint currentIndexes[size * size];
+
+                for (uint i = 0; i < size * size; ++i)
+                    currentIndexes[puzzle.grid[i]] = i;
+
+                std::unordered_set<uint> rows;
+                std::unordered_set<uint> cols;
+                for (uint i = 1; i < size * size; ++i) {
+                    uint ipx = currentIndexes[i] % size;
+                    uint ipy = currentIndexes[i] / size;
+                    uint igx = goalIndexes[i] % size;
+                    uint igy = goalIndexes[i] / size;
+                    if (rows.count(i) == 0) {
+                        for (uint x = 0; x < size; ++x) {
+                            uint j = puzzle.grid[ipy * size + x];
+                            if (i != j && rows.count(j) == 0) {
+                                uint jpx = currentIndexes[j] % size;
+                                uint jpy = currentIndexes[j] / size;
+                                uint jgx = goalIndexes[j] % size;
+                                uint jgy = goalIndexes[j] / size;
+                                if (ipy == igy == jpy == jgy && ((ipx < jpx && igx > jgx) || (ipx > jpx && igx < jgx))) {
+                                    rows.insert(i);
+                                    rows.insert(j);
+                                    cost += 2;
+                                }
+                            }
+                        }
+                    }
+                    if (cols.count(i) == 0) {
+                        for (uint y = 0; y < size; ++y) {
+                            uint j = puzzle.grid[y * size + ipx];
+                            if (i != j && cols.count(j) == 0) {
+                                uint jpx = currentIndexes[j] % size;
+                                uint jpy = currentIndexes[j] / size;
+                                uint jgx = goalIndexes[j] % size;
+                                uint jgy = goalIndexes[j] / size;
+                                if (ipx == igx == jpx == jgx && ((ipy < jpy && igy > jgy) || (ipy > jpy && igy < jgy))) {
+                                    cols.insert(i);
+                                    cols.insert(j);
+                                    cost += 2;
+                                }
+                            }
+                        }
                     }
                 }
 
@@ -33,8 +67,7 @@ namespace algorithm {
             }
 
         private:
-
-            uint indexes[size * size];
+            uint goalIndexes[size * size];
 
         };
 
