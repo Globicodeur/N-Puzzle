10d9f4acc7329dbcf4e24e13930c1a67494a00d5
Guillaume DEPARDON <guillaume.depardon@student.42.fr>
2015-05-11 19:00:47 +0200

    [Display] Better solution display

--- srcs/puzzle/Solution.hpp
+++ srcs/puzzle/Solution.hpp
@@ -18,9 +18,9 @@ namespace puzzle {
         friend std::ostream & operator<<(std::ostream & os, const Solution & solution) {
             for (const auto & puzzle: solution)
                 os << puzzle << "\n";
-            os  << "A* iterations: " << solution.timeComplexity << "\n"
-                << "Total number of states visited: " << solution.spaceComplexity << "\n"
-                << "Number of moves required: " << solution.size() - 1 << "\n";
+            os  << "Number of states selected:          " << solution.timeComplexity << "\n"
+                << "Maximum number of states in memory: " << solution.spaceComplexity << "\n"
+                << "Number of moves required:           " << solution.size() - 1 << "\n";
             return os;
         }
     };
