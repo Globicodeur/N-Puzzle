b60e627d781ebe404cd8429cd632a51bc90cb994
Guillaume DEPARDON <guillaume.depardon@student.42.fr>
2015-05-11 22:26:50 +0200

    [:lipstick:] More idiomatic composition class

--- srcs/algorithm/heuristic_composition.hpp
+++ srcs/algorithm/heuristic_composition.hpp
@@ -10,28 +10,32 @@ namespace algorithm {
         struct Composition {
 
             template <uint size>
-            struct Composer: public Heuristics<size>... {
+            class Composer: public Heuristics<size>... {
 
-                Composer(const puzzle::Puzzle<size> & goal):
+                using Puzzle = puzzle::Puzzle<size>;
+
+            public:
+
+                Composer(const Puzzle & goal):
                     Heuristics<size> { goal } ...
                 { }
 
-                uint operator()(const puzzle::Puzzle<size> & puzzle) const {
+                uint operator()(const Puzzle & puzzle) const {
                     return call<Heuristics...>(puzzle);
                 }
 
             private:
 
-                template <template <uint> class H1, template <uint> class... Hn>
-                std::enable_if_t<sizeof...(Hn) != 0, uint>
-                call(const puzzle::Puzzle<size> & puzzle) const {
-                    return call<H1>(puzzle) + call<Hn...>(puzzle);
+                template <HClass H1, HClass... Hn>
+                std::enable_if_t<sizeof...(Hn) == 0, uint>
+                call(const Puzzle & puzzle) const {
+                    return H1<size>::operator()(puzzle);
                 }
 
-                template <template <uint> class H1, template <uint> class... Hn>
-                std::enable_if_t<sizeof...(Hn) == 0, uint>
-                call(const puzzle::Puzzle<size> & puzzle) const {
-                    return H1<size>::operator()(puzzle);
+                template <HClass H1, HClass... Hn>
+                std::enable_if_t<sizeof...(Hn) != 0, uint>
+                call(const Puzzle & puzzle) const {
+                    return call<H1>(puzzle) + call<Hn...>(puzzle);
                 }
 
             };
