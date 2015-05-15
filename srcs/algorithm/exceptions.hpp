ca30a39a6f8485dc5adcea0868d9de704cc7ed33
Guillaume DEPARDON <guillaume.depardon@student.42.fr>
2015-05-15 18:22:48 +0200

    [:lipstick:] Refactored solver exceptions

--- srcs/algorithm/exceptions.hpp
+++ srcs/algorithm/exceptions.hpp
@@ -6,64 +6,80 @@ namespace algorithm {
 
     namespace error {
 
-        struct PuzzleSizeTooLarge: public std::exception {
+        struct SolverError: public std::exception {
 
-            PuzzleSizeTooLarge(std::size_t size) {
+            SolverError(const std::string & message):
+                what_ { "Solver error: " + message }
+            { }
+
+            virtual const char * what() const noexcept {
+                return what_.c_str();
+            }
+
+        private:
+
+            std::string what_;
+
+        };
+
+        struct PuzzleSizeTooLarge: public SolverError {
+
+            static auto message(std::size_t size) {
                 std::ostringstream oss;
+
                 oss << "Solver error: the puzzle size ("
                     << size << ") is too large (maximum allowed: "
                     << MAX_PUZZLE_SIZE << ")";
-                what_ = oss.str();
+
+                return oss.str();
             }
 
-            virtual const char * what() const noexcept {
-                return what_.c_str();
-            }
-
-        private:
-            std::string what_;
-        };
-
-        struct ZeroGeneration: public std::exception {
-
-            virtual const char * what() const noexcept {
-                return "Solver error: it does not make sense to generate a 0-puzzle";
-            }
+            PuzzleSizeTooLarge(std::size_t size):
+                SolverError { message(size) }
+            { }
 
         };
 
-        struct EmptyPuzzle: public std::exception {
+        struct ZeroGeneration: public SolverError {
 
-            virtual const char * what() const noexcept {
-                return "Solver error: it does not make sense to solve a 0-puzzle";
-            }
-
-        };
-
-        struct PuzzleNotSolvable: public std::exception {
-
-            virtual const char * what() const noexcept {
-                return "Solver error: the puzzle is not solvable";
-            }
+            ZeroGeneration():
+                SolverError { "it does not make sense to generate a 0-puzzle" }
+            { }
 
         };
 
-        struct GoalSizeMismatch: public std::exception {
+        struct EmptyPuzzle: public SolverError {
 
-            GoalSizeMismatch(std::size_t goalSize, std::size_t initialSize) {
+            EmptyPuzzle():
+                SolverError { "it does not make sense to solve a 0-puzzle" }
+            { }
+
+        };
+
+        struct PuzzleNotSolvable: public SolverError {
+
+            PuzzleNotSolvable():
+                 SolverError { "the puzzle is not solvable" }
+            { }
+
+        };
+
+        struct GoalSizeMismatch: public SolverError {
+
+            static auto message(std::size_t goalSize, std::size_t initialSize) {
                 std::ostringstream oss;
+
                 oss << "Solver error: the goal size ("
                     << goalSize << ") does not match the initial state size ("
                     << initialSize << ")";
-                what_ = oss.str();
+
+                return oss.str();
             }
 
-            virtual const char * what() const noexcept {
-                return what_.c_str();
-            }
+            GoalSizeMismatch(std::size_t goalSize, std::size_t initialSize):
+                SolverError { message(goalSize, initialSize) }
+            { }
 
-        private:
-            std::string what_;
         };
 
     }
